d1e874bd668bd57e3ec90b5bf6dc8ecbb59c8869
Camila Vilarinho <camilaavilarinho@gmail.com>
2018-02-11 18:45:52 +0000

    Add e2e tests

---
+++ test/e2e/searchCity.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const expect = chai.expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+const webdriver = require('selenium-webdriver');
+
+const driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+describe('Search form', function () {
+    this.timeout(10000);
+
+    before(function (done) {
+        driver.navigate().to(' http://localhost:3000/')
+            .then(() => done())
+    });
+
+    it('search for a city\'s weather forecast', function (done) {
+        driver.findElement({ css: '.search' }).sendKeys('london');
+        driver.findElement({ css: '.btn' }).click();
+        return expect(driver.findElement({ css: '.city' }).getText()).to.eventually.equal('London, GB');
+    });
+
+    after(function (done) {
+        driver.quit()
+            .then(() => done())
+    });
+});
